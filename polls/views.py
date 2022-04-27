86cee6998d696c9d4542df8145fb126c46375eb4
fede-Aranda <arandafederico10@gmail.com>
2022-04-27 16:29:51 -0300

    segundo commit, redireccion de pagina y guyot y sus olores

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index. Guyot tiene olor a pata, Hace una semana que no se baña ")
+    

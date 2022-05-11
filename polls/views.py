57d040389bdb3f9896bffe05a5a82d31d12792e1
fede-Aranda <arandafederico10@gmail.com>
2022-05-11 14:08:39 -0300

    feature - agrego archivos

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. Guyot tiene olor a pata, Hace una semana que no se baña ")
-    
+    return HttpResponse("Hello, world. You're at the polls index. \n Guyot tiene olor a pata, \n Hace una semana que no se baña ")
+    return render(request,index.html)
